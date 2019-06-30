a146de7a26a97083944d5e2e9f5ae2b35fdf338d
wx <1286456676@qq.com>
2019-06-30 11:43:47 +0800

    容器

---
+++ src/cn/sxt/mycollection/Node2.java
@@ -0,0 +1,11 @@
+package cn.sxt.mycollection;
+
+//用于SxtHashMap中
+public class Node2 {
+	
+	int  hash;
+	Object  key;
+	Object  value;
+	Node2  next; 
+	
+}
---
+++ src/cn/sxt/mycollection/Node3.java
@@ -0,0 +1,11 @@
+package cn.sxt.mycollection;
+
+//用于SxtHashMap中
+public class Node3<K,V> {
+	
+	int  hash;
+	K  key;
+	V  value;
+	Node3  next; 
+	
+}
